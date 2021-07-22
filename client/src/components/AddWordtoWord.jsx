603c430c5d3dd1946db9ebfd02900ac0aa251d0e
Aras Uludag <arasuludag@gmail.com>
2021-07-22 18:21:30 +0300

    Implement enter key to adding words

--- client/src/components/AddWordtoWord.jsx
+++ client/src/components/AddWordtoWord.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
-import { useHistory } from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 
 import Input from "./Input.jsx";
 import RadioButton from "./RadioButton.jsx";
@@ -15,8 +15,8 @@ import Snackbar from "./Snackbar.jsx";
 
 const CustomDialog = withStyles({
   paper: {
-    backgroundColor: "#1c1d26",
-  },
+    backgroundColor: "#1c1d26"
+  }
 })(Dialog);
 
 function FormDialog(props) {
@@ -39,16 +39,20 @@ function FormDialog(props) {
   const [word, setWord] = useState("");
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+
+    try {
+      event.preventDefault();
+    } catch  {};
 
     const addedWord = {
       word: word,
       relation: radio,
-      tab: props.tabData[props.index]._id,
+      tab: props.tabData[props.index]._id
     };
 
-    axios.post("/api/add-word-to-word", { addedWord }).then((res) => {
-        setSnackbar(true);
+    axios.post("/api/add-word-to-word", {addedWord}).then((res) => {
+      setSnackbar(true);
+      setWord("");
     });
   };
 
@@ -56,49 +60,38 @@ function FormDialog(props) {
     setWord(event.target.value);
   };
 
-  return (
-    <div>
-      <Button
-        variant="contained"
-        size="small"
-        onClick={handleClickOpen}
-        style={{
-          backgroundColor: "#650073",
-          color: "white",
-          borderRadius: "33px",
-          marginTop: "50px",
-        }}
-      >
-        Kelime Ekle
-      </Button>
-      <CustomDialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Kelime Ekle</DialogTitle>
-        <DialogContent>
-          <RadioButton props={(value) => setRadio(value)} />
-          <Input type="text" onChange={handleChange} label="Yaz" />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="secondary">
-            İptal
-          </Button>
-          <Button onClick={handleSubmit} color="secondary">
-            Ekle Gitsin
-          </Button>
-        </DialogActions>
-      </CustomDialog>
-      <Snackbar
-  snackbar={snackbar}
-  setSnackbar={(bool) => {
-    setSnackbar(bool);
-  }}
-/>
+  return (<div>
+    <Button variant="contained" size="small" onClick={handleClickOpen} style={{
+        backgroundColor: "#650073",
+        color: "white",
+        borderRadius: "33px",
+        marginTop: "50px"
+      }}>
+      Kelime Ekle
+    </Button>
+    <CustomDialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title">Kelime Ekle</DialogTitle>
+      <DialogContent>
+        <RadioButton props={(value) => setRadio(value)}/>
+        <Input type="text" onKeyUp={(event) => {
+            if (event.key === 'Enter')
+              handleSubmit();
+            }} onChange={handleChange} label="Yaz" value={word}/>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="secondary">
+          İptal
+        </Button>
+        <Button type="submit" onClick={handleSubmit} color="secondary">
+          Ekle Gitsin
+        </Button>
+      </DialogActions>
+    </CustomDialog>
+    <Snackbar snackbar={snackbar} setSnackbar={(bool) => {
+        setSnackbar(bool);
+      }}/>
 
-    </div>
-  );
+  </div>);
 }
 
 export default FormDialog;
